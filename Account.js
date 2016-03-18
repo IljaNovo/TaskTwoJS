ee30122c80b0f83a3e2d516211bb398ad3a60c81
Ilya Novozhilov <ilja.keinn@mail.ru>
2016-03-18 05:55:14 +0300

    create files of classes

---
+++ Account.js
@@ -0,0 +1,8 @@
+function Account (accountName) {
+    this._accountName = accountName;
+    this._person;
+    this._lastOperation;
+    this._balance = 0;
+    this._isActivated = false;
+}
+
---
+++ transferService.js
@@ -0,0 +1,14 @@
+var transferService = (function () {
+    
+    function publicRegister (person, account, balance) {
+        
+    }
+    
+    function publicTransfer (accountFrom, accountTo, sum) {
+        
+    }
+    
+    function publicGetPersonAccounts(person) {
+        
+    }
+})
