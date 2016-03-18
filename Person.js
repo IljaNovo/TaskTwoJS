721caee963da7f7a0439ee8a723d48a6a93bc793
Ilya Novozhilov <ilja.keinn@mail.ru>
2016-03-18 09:00:03 +0300

    create class Person

--- Person.js
+++ Person.js
@@ -1,5 +1,33 @@
-function Person (name, birthDate) {
+function Person (name, birthDateStr) {
     this._name = name;
-    this._birthDate = birthDate;
+    this._birthData = new Date();
+    parseBirthDate(this._birthData, birthDateStr);
     this._accounts = new Array();
+}
+
+Object.defineProperty(Person.prototype, "name", {
+    get: function () {
+        return this._name;
+}})
+
+Object.defineProperty(Person.prototype, "birthDate", {
+    get: function () {
+        console.log("Год:" + this._birthData.getFullYear() +
+                " Месяц: " + (this._birthData.getMonth() + 1) +
+                " День: " + this._birthData.getDate());
+}})
+
+Person.prototype.addAccount = function(account) {
+    this._accounts.push(account);
+}
+
+Person.prototype.getAccounts = function() {
+    return this._accounts;
+}
+
+function parseBirthDate(birthDate, dateStr) {
+    var dayMonthYear = dateStr.split("/");
+    birthDate.setFullYear(Number(dayMonthYear[0]));
+    birthDate.setMonth(Number(dayMonthYear[1]) - 1);
+    birthDate.setDate(Number(dayMonthYear[2]));
 }
